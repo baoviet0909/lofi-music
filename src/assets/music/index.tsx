053254622f604bacbda016dc0b24bc7de96c6a70
LeHoaiThanh1603 <lehoaithanhcd005@gmail.com>
2023-02-18 15:31:53 +0700

    push asset

---
+++ src/assets/music/index.tsx
@@ -0,0 +1,13 @@
+import music1 from "./1.mp3";
+import music2 from "./2.mp3";
+import music3 from "./3.mp3";
+import music4 from "./4.mp3";
+import music5 from "./5.mp3";
+import music6 from "./6.mp3";
+import music7 from "./7.mp3";
+import music8 from "./8.mp3";
+import music9 from "./9.mp3";
+import music10 from "./10.mp3";
+import music11 from "./11.mp3";
+
+export { music1, music2, music3, music4, music5, music6, music7, music8, music9, music10, music11 };
---
+++ src/assets/music/vn/index.tsx
@@ -0,0 +1,13 @@
+import music_1 from "./song-1.mp3";
+import music_2 from "./song-2.mp3";
+import music_3 from "./song-3.mp3";
+import music_4 from "./song-4.mp3";
+import music_5 from "./song-5.mp3";
+import music_6 from "./song-6.mp3";
+import music_7 from "./song-7.mp3";
+import music_8 from "./song-8.mp3";
+import music_9 from "./song-9.mp3";
+import music_10 from "./song-10.mp3";
+import music_11 from "./song-11.mp3";
+
+export { music_1, music_2, music_3, music_4, music_5, music_6, music_7, music_8, music_9, music_10, music_11 };
